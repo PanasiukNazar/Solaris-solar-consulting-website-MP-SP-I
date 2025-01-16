6055c1cfd9e3b574487d2748ef15b80062f0b7a6
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2025-01-16 11:54:42 +0200

    WIP

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function ($) {
-    "use strict";
+    'use strict';
 
     // Spinner
     var spinner = function () {
@@ -10,100 +10,96 @@
         }, 1);
     };
     spinner();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
-
     // Team carousel
-    $(".team-carousel").owlCarousel({
+    $('.team-carousel').owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
         center: false,
         dots: false,
         loop: true,
         margin: 50,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="bi bi-arrow-right"></i>',
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1,
             },
-            768:{
-                items:2
+            768: {
+                items: 2,
             },
-            992:{
-                items:3
-            }
-        }
+            992: {
+                items: 3,
+            },
+        },
     });
-
 
     // Testimonial carousel
 
-    $(".testimonial-carousel").owlCarousel({
+    $('.testimonial-carousel').owlCarousel({
         autoplay: true,
         smartSpeed: 1500,
         center: true,
         dots: true,
         loop: true,
         margin: 0,
-        nav : true,
+        nav: true,
         navText: false,
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1,
             },
-            576:{
-                items:1
+            576: {
+                items: 1,
             },
-            768:{
-                items:2
+            768: {
+                items: 2,
             },
-            992:{
-                items:3
-            }
-        }
+            992: {
+                items: 2,
+            },
+        },
     });
 
+    // Fact Counter
 
-     // Fact Counter
-
-     $(document).ready(function(){
-        $('.counter-value').each(function(){
-            $(this).prop('Counter',0).animate({
-                Counter: $(this).text()
-            },{
-                duration: 2000,
-                easing: 'easeInQuad',
-                step: function (now){
-                    $(this).text(Math.ceil(now));
-                }
-            });
+    $(document).ready(function () {
+        $('.counter-value').each(function () {
+            $(this)
+                .prop('Counter', 0)
+                .animate(
+                    {
+                        Counter: $(this).text(),
+                    },
+                    {
+                        duration: 2000,
+                        easing: 'easeInQuad',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        },
+                    },
+                );
         });
     });
-
-
-
 })(jQuery);
-
